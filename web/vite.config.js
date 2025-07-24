3c3058a340e4f6306836b7658a3893e52cc3a3e1
StafLoker <dev.stafloker@gmail.com>
2025-07-24 13:08:25 +0300

    refactor: remove svg sprite & vue devtools dependencies

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,21 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import createSvgSpritePlugin from 'vite-plugin-svg-sprite'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
     plugins: [
       vue(),
-      vueDevTools(),
       tailwindcss(),
-      createSvgSpritePlugin({
-        exportType: 'vue',
-        include: '**/icons/*.svg',
-      }),
     ],
     resolve: {
       alias: {
